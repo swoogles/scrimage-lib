83148757e96af93d9154790f1b6e6c8c4e31e3e0
bfrasure <bill@solutionreach.com>
2016-12-05 14:03:36 -0700

    Combine pprint & text wrapping for Map images  #time 1h This will be handy for show the acc part of fold/scan operations.

--- src/main/scala/com/billding/imagemanipulation/ScrimageFun.scala
+++ src/main/scala/com/billding/imagemanipulation/ScrimageFun.scala
@@ -21,15 +21,43 @@ import com.sksamuel.scrimage.Directory
 // Needed for SnowFilter for some weird reason.
 import scala.concurrent.ExecutionContext.Implicits.global
 
-import com.sksamuel.scrimage.canvas.drawable.Rect
-import com.sksamuel.scrimage.canvas.Drawable
-
 import pprint.Config
+
+trait TextDrawing {
+  import ammonite.ops._
+  val imgFont = new JFont("Sans-seriff", 1, 28)
+  val generatedImgDir = cwd / 'GeneratedImages
+  def makeTextDrawable(content: List[String]): List[Text] = {
+    makeTextDrawable(content, 20, 30)
+  }
+
+  def makeTextDrawable(content: List[String], x: Int, y: Int): List[Text] = {
+    content.zipWithIndex.map { case (lineContent, lineIdx) =>
+      Text(lineContent, x, y + (lineIdx * 30), { g2 =>
+        g2.setBackground(JColor.WHITE)
+        g2.setFont(imgFont)
+      })
+    }
+  }
+
+  def makeImgFromText(content: List[String], sequenceIdx: Int = 0) = {
+    val drawableText = makeTextDrawable(content)
+    val img: Canvas = Image(1400, 800)
+      .fit(1400, 800, Color.Black)
+      .pad(100, Color.Black)
+    val imgPath = generatedImgDir / (s"commit_${sequenceIdx}_.jpg")
+    val imgWithText: Canvas = drawableText.foldLeft(img){
+      case (curImg: Canvas, nextText: Text) => curImg.draw(nextText)
+    }
+    imgWithText.output(imgPath.toIO)(JpegWriter())
+  }
+
+}
 
 /**
   * Created by bfrasure on 11/15/16.
   */
-object ScrimageFun {
+object ScrimageFun extends TextDrawing {
   import ammonite.ops._
 
   implicit val pprintConfig = Config()
@@ -37,7 +65,6 @@ object ScrimageFun {
   val originalImgDir = ammonite.ops.pwd / 'OriginalImages
   val listed = ls! originalImgDir
   val manipulatedImgDir = cwd / 'ManipulatedImages
-  val generatedImgDir = cwd / 'GeneratedImages
 
   def copyFreshImages_!(srcDir: Path, targetDir: Path): LsSeq = {
     val oldImages = ls! targetDir
@@ -55,23 +82,11 @@ object ScrimageFun {
 //    ls! targetDir
   }
 
-  val imgFont = new JFont("Sans-seriff", 1, 28)
-
   val imgTextNew =
     Text("Happy Holidays!", 200, 800-20, { g2 =>
       g2.setBackground(JColor.WHITE)
       g2.setFont(imgFont)
     })
-
-  def makeTextDrawable(content: List[String]) = {
-    content.zipWithIndex.map { case (lineContent, lineIdx) =>
-      Text(lineContent, 20, 30 + (lineIdx * 30), { g2 =>
-        g2.setBackground(JColor.WHITE)
-        g2.setFont(imgFont)
-      })
-    }
-  }
-
 
   val borderFunc: (Image=>Image) = (imgInner) => {
     val totalBorderSize = 150
@@ -134,183 +149,5 @@ object ScrimageFun {
       makeImgFromText(curRevision, idx)
     }
 
-  def makeImgFromText(content: List[String], sequenceIdx: Int = 0) = {
-    val drawableText = makeTextDrawable(content)
-    val img: Canvas = Image(1400, 800)
-      .fit(1400, 800, Color.Black)
-      .pad(100, Color.Black)
-    val imgPath = generatedImgDir / (s"commit_${sequenceIdx}_.jpg")
-    val imgWithText: Canvas = drawableText.foldLeft(img){
-      case (curImg: Canvas, nextText: Text) => curImg.draw(nextText)
-    }
-    imgWithText.output(imgPath.toIO)(JpegWriter())
-  }
-
-  def drawSomeSquares() = {
-    val startingTextBox = HeadLongTextListItem("801-971-9844")
-    // case class TailLongTextListItem(content: String, prevItem: LongItem, value: Int = 1) extends LongItem {
-
-    val typedItems = Range(1, 6).map { curIdx =>
-      NumericalListItem(
-        Rect(x=200+100*curIdx, y=50, width=50, height=50, { g2 =>
-          g2.setColor(JColor.GREEN)
-          g2.setFont(imgFont)
-        }) ,
-        curIdx
-      )
-    }
-
-
-     val accumulator = NumericalListItem(Rect(x=50, y=50, width=50, height=50), 0)
-
-     val typedListsB = typedItems.scanLeft((accumulator, typedItems)){
-       case ((acc, remainingItems), nextItem) => 
-         (
-           acc
-             .copy(
-             rect=acc.rect.copy(
-               y=acc.rect.y+100
-             ),
-             value=acc.value+nextItem.value
-           ), 
-           remainingItems.tail.map(li=>
-               li.copy(
-                 rect=li.rect.copy(
-                   y=li.rect.y+100
-                 )
-               )
-             )
-           )
-     }
-
-    val img: Canvas = Image(1400, 800)
-      .fit(1400, 800, Color.Black)
-    val imgPath = generatedImgDir / (s"rectangles.jpg")
-
-    val scannedDrawables = typedListsB.flatMap { tup => tup._1 +: tup._2 }
-    pprint.pprintln(scannedDrawables)
-
-    val imgWithScannedDrawables = scannedDrawables.foldLeft(img){
-      case (curImg: Canvas, li: CustomDrawable) => curImg.draw(li.rect).draw(li.text)
-    }
-
-    imgWithScannedDrawables.output(imgPath.toIO)(JpegWriter())
-  }
-
-  def phoneNumbers() = {
-    val areaCodesAndStates = Map(
-      "801"->"UT",
-      "865"->"TN"
-    )
-    val phoneNumbers = List(
-      "801-971-9844",
-      "800-222-3333",
-      "865-104-1623",
-      "865-200-3273",
-      "600-222-3333"
-    )
-    val typedPhoneList = phoneNumbers.foldLeft(HeadLongTextListItem("000-000-0000"): LongItem){
-      case (acc, curNumber) => TailLongTextListItem(curNumber, acc)
-    }
-
-    val typedPhoneNumbers = phoneNumbers.zipWithIndex.map { case (number, idx) =>
-      PhoneNumberListItem(
-        number,
-        Rect(x=200+100*idx, y=50, width=50, height=50, { g2 =>
-          g2.setColor(JColor.GREEN)
-          g2.setFont(imgFont)
-        })
-      )
-    }
-
-    val organizedNumbers = Map[String, List[String]]().withDefaultValue(List(): List[String])
-    val updatedMap = (organizedNumbers
-      + ("UNKNOWN" -> List())
-      + ("UT" -> List("911"))
-      + ("CO" -> List("100-200-3000"))
-      )
-    pprint.pprintln(updatedMap)
-
-    val locationFolding = typedPhoneNumbers.foldLeft(updatedMap){ case (acc, li) =>
-      val lookupRes: Option[String] = areaCodesAndStates.get(li.phoneNumber.take(3))
-      val region = lookupRes.getOrElse("UNKNOWN")
-      val neighboringEntries = acc(region)
-      acc + (region -> (li.phoneNumber :: neighboringEntries))
-    }
-
-
-    val locationLookups = typedPhoneNumbers.map{ case li: PhoneNumberListItem =>
-        areaCodesAndStates.get(li.phoneNumber.take(3))
-    }.flatten.toSet
-
-    pprint.pprintln(locationFolding)
-  
-
-    val img: Canvas = Image(1400, 800)
-      .fit(1400, 800, Color.Black)
-    val imgPath = generatedImgDir / (s"phone_folding.jpg")
-
-    val scannedDrawables = LongItem.allDrawables(typedPhoneList)
-    pprint.pprintln(scannedDrawables)
-
-    val imgWithScannedDrawables = scannedDrawables.foldLeft(img){
-      case (curImg: Canvas, li: Drawable) => curImg.draw(li)
-    }
-
-    imgWithScannedDrawables.output(imgPath.toIO)(JpegWriter())
-  }
 }
 
-sealed trait CustomDrawable {
-  val rect: Rect
-  val value: Int
-  val content: String
-  val imgFont = new JFont("Sans-seriff", 1, 28)
-
-  def text: Text =
-       Text(content.toString, rect.x+15, rect.y+30, { g2 =>
-         g2.setBackground(JColor.BLUE)
-         g2.setFont(imgFont)
-       })
-
-}
-
-sealed trait LongItem extends CustomDrawable {
-  override val imgFont = new JFont("Sans-seriff", 1, 14)
-}
-
-object LongItem {
-  def allDrawables(li: LongItem): List[Drawable] = li match {
-    case head: HeadLongTextListItem => List(head.rect, head.text)
-    case tail: TailLongTextListItem => List(tail.rect, tail.text) ::: allDrawables(tail.prevItem)
-  }
-}
-
-case class HeadLongTextListItem(content: String, value: Int = 1) extends LongItem {
-  val rect =
-        Rect(x=100, y=50, width=150, height=50)
-}
-
-case class TailLongTextListItem(content: String, prevItem: LongItem, value: Int = 1) extends LongItem {
-  val rect =
-        Rect(x=prevItem.rect.x+200, y=50, width=150, height=50)
-}
-
-
-case class NumericalListItem(rect: Rect, value: Int = 1) extends CustomDrawable {
-  val content = value.toString
-}
-
-case class ListItem(rect: Rect, label: Char, value: Int = 1) extends CustomDrawable {
-  val content = label.toString
-  override val text =
-       Text(label.toString, rect.x+15, rect.y+30, { g2 =>
-         g2.setColor(JColor.BLUE)
-         g2.setBackground(JColor.WHITE)
-         g2.setFont(imgFont)
-       })
-}
-
-case class PhoneNumberListItem(phoneNumber: String, rect: Rect, value: Int = 1) extends CustomDrawable {
-  val content = phoneNumber
-}
--- src/test/scala/imagemanipulation/ScrimageFunTests.scala
+++ src/test/scala/imagemanipulation/ScrimageFunTests.scala
@@ -4,9 +4,17 @@ import org.scalatest.FlatSpec
 
 class ScrimageFunTests extends FlatSpec {
 
-  "scrimage" should "create images for every step" in {
-    ScrimageFun.phoneNumbers()
-    println("pointless test")
+  "scrimage" should "show all the steps in a fold summation" in {
+    Transformations.foldSummationImage()
   }
+
+  "scrimage" should "create phone number fold demo image" in {
+    Transformations.phoneNumbers()
+  }
+
+  "scrimage" should "draw a Map image" in {
+    Transformations.drawMapImage()
+  }
+
 
 }
